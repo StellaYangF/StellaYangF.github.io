fd4dd83d837a5003713e81ab485dc96de49acd61
Stella <stellayangfan@gmail.com>
2023-07-18 10:45:55 +0800

    docs: classify outlet

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -7,20 +7,46 @@ export default defineConfig({
   base: "/stella/",
   description: "Frontend technology learning notes.",
   themeConfig: {
+    search: {
+      provider: 'local'
+    },
     nav: [
       { text: '概览', link: '/guide' },
-      { text: '前端', link: '/front/', activeMatch: '/front/axios' },
-      { text: '后端', link: '/back/', activeMatch: '/back/express' },
-      { text: '数据结构与算法', link: '/algorithm/' },
-      { text: '技术汇总', link: '/overview/' }
+      { text: '前端', link: '/front/', activeMatch: '/front/' },
+      { text: '后端', link: '/back/', activeMatch: '/back/' },
+      { text: '数据结构与算法', link: '/algorithm/' }
     ],
 
     sidebar: {
       '/front': [
         {
-          text: '前端', collapsed: false, items: [
+          text: '框架', collapsed: false, items: [
+            { text: 'vue3', link: '/front/vue3/design' },
             { text: 'axios', link: '/front/axios' },
-            { text: 'vue', link: '/front/vue' }
+          ]
+        },
+        {
+          text: '工程化', collapsed: false, items: [
+            { text: 'webpack', link: '/front/webpack/' },
+            { text: 'rollup', link: '/front/rollup/' },
+            { text: 'vite', link: '/front/vite/' }
+          ]
+        },
+        {
+          text: '性能', collapsed: false, items: [
+            { text: '性能优化', link: '/front/performance/' }
+          ]
+        },
+        {
+          text: '测试', collapsed: false, items: [
+            { text: '测试工具', link: '/front/test/' }
+          ]
+        },
+        {
+          text: '技术汇总', collapsed: false, items: [
+            { text: 'DOM 事件', link: '/front/overview/' },
+            { text: 'JavaScript', link: '/front/overview/js' },
+            { text: 'CSS', link: '/front/overview/css' }
           ]
         }
       ],
@@ -28,12 +54,6 @@ export default defineConfig({
         { text: 'express', link: '/back/express' },
         { text: 'koa', link: '/back/koa' },
         { text: 'websocket', link: '/back/websocket' },
-      ],
-      '/overview': [
-        { text: 'DOM 事件', link: '/overview/' },
-        { text: '性能优化', link: '/overview/performance' },
-        { text: 'JavaScript', link: '/overview/js' },
-        { text: 'CSS', link: '/overview/css' }
       ]
     },
     socialLinks: [
